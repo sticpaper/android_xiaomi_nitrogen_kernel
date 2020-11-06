7b3c713fcab185ae4e8f711e694c9dbdde16df8e
andip71 <andreasp@gmx.de>
2020-11-06 19:35:27 +0800

    boeffla_wl_blocker: add generic wakelock blocker driver v2.0

---
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -0,0 +1,18 @@
+/*
+ * Author: andip71, 01.09.2017
+ *
+ * Version 2.0
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ */
+
+#define BOEFFLA_WL_BLOCKER_VERSION	"2.0"
+
+#define LIST_WL_DEFAULT			"NETLINK"
+
+#define LENGTH_LIST_WL			255
+#define LENGTH_LIST_WL_DEFAULT		107
+#define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
