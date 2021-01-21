ee9c82126ecbf74f9b3dd88d57da7d2b6798b457
Aquarius223 <2777968262@qq.com>
2021-01-21 09:19:17 +0800

    drivers: usb-gadget: Check out mass_storage to create a device structure
    Conveniently identify the phone as "USB mass storage"
    It can be used with the following software: https://github.com/outofmemo/UMS-Interface
    
    test:
    /sys/class/android_usb/android0/f_mass_storage load normally.

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -227,7 +227,7 @@
 
 /*------------------------------------------------------------------------*/
 
-#define FSG_DRIVER_DESC		"Mass Storage Function"
+#define FSG_DRIVER_DESC		"mass_storage"
 #define FSG_DRIVER_VERSION	"2009/09/11"
 
 static const char fsg_string_interface[] = "Mass Storage";
@@ -3479,6 +3479,9 @@ static void fsg_free_inst(struct usb_function_instance *fi)
 	kfree(opts);
 }
 
+/* Create mass_storage device structure */
+extern struct device *create_function_device(char *name);
+
 static struct usb_function_instance *fsg_alloc_inst(void)
 {
 	struct fsg_opts *opts;
@@ -3517,6 +3520,9 @@ static struct usb_function_instance *fsg_alloc_inst(void)
 	opts->func_inst.group.default_groups = opts->default_groups;
 
 	config_group_init_type_name(&opts->func_inst.group, "", &fsg_func_type);
+
+    /* Create a virtual device */
+	create_function_device("f_mass_storage");
 
 	return &opts->func_inst;
 
